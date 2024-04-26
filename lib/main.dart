b8b8cb488d5986516d8446362948c7d930e42fa5
Simon Gerstmeier <simon@gerstmeier.dev>
2024-04-26 03:56:07 +0200

    Set the native window title to "ezBadminton"

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:window_manager/window_manager.dart';
 import 'app.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // Must add this line.
+  await windowManager.ensureInitialized();
+
+  await windowManager.setTitle("ezBadminton");
+
   runApp(const App());
 }
