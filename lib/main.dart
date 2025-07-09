d5b4f3716660c180e5c7630b93bcde46d0285bab
Simon Gerstmeier <simon@gerstmeier.dev>
2025-07-09 21:01:10 +0200

    Added a --version flag to print the client's version

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:args/args.dart';
 import 'package:ez_badminton_admin_app/tls_cert_override.dart';
 import 'package:flutter/material.dart';
+import 'package:logger/logger.dart';
 import 'package:window_manager/window_manager.dart';
 
 import 'package:ez_badminton_admin_app/assets/pdf_fonts.dart';
@@ -10,15 +11,30 @@ import 'package:ez_badminton_admin_app/assets/pdf_fonts.dart';
 import 'app.dart';
 
 void main(List<String> arguments) async {
+  final argParser = ArgParser()
+    ..addOption("address")
+    ..addFlag("version", defaultsTo: false, abbr: "v");
+  ArgResults argResults = argParser.parse(arguments);
+
+  bool showVersion = argResults.flag("version");
+  if (showVersion) {
+    const String version =
+        String.fromEnvironment("VERSION", defaultValue: "dev");
+    Logger logger = Logger(
+      filter: ProductionFilter(),
+      printer: SimplePrinter(),
+    );
+    logger.i('ezBadminton client version: $version');
+    exit(0);
+  }
+
+  String? ip = argResults.option("address");
+
   WidgetsFlutterBinding.ensureInitialized();
   await PdfFonts.ensureInitialized();
   await windowManager.ensureInitialized();
 
   await windowManager.setTitle("ezBadminton");
-
-  final argParser = ArgParser()..addOption("address");
-  ArgResults argResults = argParser.parse(arguments);
-  String? ip = argResults.option("address");
 
   HttpOverrides.global = PrivateIPCertOverride();
 
