ad02dacea8956e6c1b10f3136d4f766865e17c8d
Simon Gerstmeier <simon@gerstmeier.dev>
2025-07-09 21:36:56 +0200

    Added --username and --password options to log in via command line

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,9 @@ import 'app.dart';
 void main(List<String> arguments) async {
   final argParser = ArgParser()
     ..addOption("address")
-    ..addFlag("version", defaultsTo: false, abbr: "v");
+    ..addFlag("version", defaultsTo: false, abbr: "v")
+    ..addOption("username", abbr: "u")
+    ..addOption("password", abbr: "p");
   ArgResults argResults = argParser.parse(arguments);
 
   bool showVersion = argResults.flag("version");
@@ -30,6 +32,8 @@ void main(List<String> arguments) async {
   }
 
   String? ip = argResults.option("address");
+  String? username = argResults.option("username");
+  String? password = argResults.option("password");
 
   WidgetsFlutterBinding.ensureInitialized();
   await PdfFonts.ensureInitialized();
@@ -51,7 +55,11 @@ void main(List<String> arguments) async {
 
   HttpOverrides.global = PrivateIPCertOverride();
 
-  runApp(App(ip: ip));
+  runApp(App(
+    ip: ip,
+    username: username,
+    password: password,
+  ));
 }
 
 void _onKey(KeyEvent event) async {
